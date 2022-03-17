1fa03fb46a28222c99bcd9b0fb053807d3b1580c
Elton Ramon <elton_dutra88@hotmail.com>
2022-03-17 19:06:05 -0300

    Conhecendo os operadores da linguagem py.

---
+++ operadores.py
@@ -0,0 +1,9 @@
+#Neste programa iremos praticar alguns operadores desta linguagem.
+#Operadores de atribuição: Serve para incluir/associar valores a uma variável/objeto.
+
+ #atribuição da soma de dois números inteiros
+resultado = 2+3
+print("Resultado: ",resultado)
+
+#
+
