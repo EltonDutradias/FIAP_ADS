8c09a07cbf483909b22931ab10e9ba161c32f316
elton <elton.dutradias.ti@gmail.com>
2022-03-17 22:20:32 -0300

    Conhecendo operadores de py

---
+++ operadores_atribuicao.py
@@ -0,0 +1,19 @@
+#Operadores de py.
+
+ #Operadores de atribuição: Servem para incluir/associar valores a uma variável/objeto.
+ #atribuição da soma de dois números inteiros.
+resultado = 2 + 3
+print("Resultado: ",resultado)
+
+#atribuição de um texto (string)
+nome = "Pedro"
+print("Nome: ", nome )
+nome2 = "Maria"
+#saida f"string" com a variável entre as chaves.
+print(f"Olá,{nome2}. Tudo bem?")
+
+#somando dois ao valor anterior da variável.
+resultado += 2
+print("Resultado: ", resultado)
+
+
