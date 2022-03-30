eaf67a3ddc61980d95dcaa828fbd79c939eac490
elton <elton.dutradias.ti@gmail.com>
2022-03-29 21:44:03 -0300

    Operadores relacional

---
+++ operador_relacional.py
@@ -0,0 +1,9 @@
+##Quando precisamos realizar comparações, é a hora de utilizarmos os operadores relacionais ou de comparações.
+print(5 == 5, "IGUAL")
+print(1 != 5, "Diferente")
+print(1 > 2, "Maior que")
+print( 1 < 2, "Menor que")
+print(1 >= 2, "Maior igual")
+print(2 <= 2 , "Menor igual")
+print(1 == True)
+print(0 == false )
