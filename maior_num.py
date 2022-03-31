64f08d78a1ac3fc7b8db9da135b83bb52438405c
elton <elton.dutradias.ti@gmail.com>
2022-03-31 14:53:53 -0300

    Desvio Condicional Composto

--- maior_num.py
+++ maior_num.py
@@ -2,5 +2,5 @@ num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
 if num1 > num2:
     print("Maior número: ", num1)
-elif num1<num2:
+else:
     print("Maior número: ", num2)
