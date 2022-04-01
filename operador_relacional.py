8f31a11878376feef765341cca5d3aba763b2996
Elton Ramon Dutra Dias <60240095+EltonDutradias@users.noreply.github.com>
2022-03-31 21:13:39 -0300

    Operadores relacionais são bastantes usados em desvios na programação.

--- operador_relacional.py
+++ operador_relacional.py
@@ -1,9 +1,12 @@
 ##Quando precisamos realizar comparações, é a hora de utilizarmos os operadores relacionais ou de comparações.
-print(5 == 5, "IGUAL")
-print(1 != 5, "Diferente")
-print(1 > 2, "Maior que")
-print( 1 < 2, "Menor que")
-print(1 >= 2, "Maior igual")
-print(2 <= 2 , "Menor igual")
-print(1 == True)
-print(0 == false )
+
+
+
+print(5 == 5, " 5 == 5, Comparação se 5 é realmente IGUAL a 5")
+print(1 != 5, "1 !=5, Comparação se 1 é Diferente de 5")
+print(1>2, "1 > 2, Comparação se 1 é maior que 2")
+print(1 < 2, "1 < 2, Comparação se 1 é menor que 2")
+print(1 >= 2, "1 >= 2, Comparação se 1 é maior ou igual a 2")
+print(2 <= 2 , "2<=2 , Comparação se 2 é Menor igual a 2")
+print(1 == True, "1 == true, Cmoparação se 1 é verdadeiro True ")
+print(0 == False, "0 == False, Comparação se 0 é Falso" )
