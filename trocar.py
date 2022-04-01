8839e2fbf7cba26105badc0bfb9b2032a9f758b5
Elton Ramon Dutra Dias <EltonDutradias@users.noreply.github.com>
2022-04-01 17:18:47 -0300

    Com a utlização de variável temporária, podemos realizar a troca de dados entre as variáveis principais. Realizando tráfego dos dados entre variáveis.

---
+++ trocar.py
@@ -0,0 +1,8 @@
+#Exercitando lógica, trocando valores entre variáveis. 
+print("Este programa inverte os conteúdos de duas variáveis") 
+A = input("Digite o conteúdo da variável A: ") 
+B = input("Digite o conteúdo da variável B: ")  
+troca = A
+A = B 
+B = troca 
+print("Agora que trocamos, a variável A contém {} e a variável B contém {}".format(A,B))
